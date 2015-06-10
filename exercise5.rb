1b706cccfec6e036dd14c4336c6b8faf98d59419
Bryan <brymac@gmail.com>
2015-06-10 13:49:16 -0400

    Version 1 of temperature converter

---
+++ exercise5.rb
@@ -0,0 +1,10 @@
+def temp_to_c(temp_orig)
+  (temp_orig  - 32) * (5 / 9)
+end
+
+puts "Please enter a temperature in Farenheit"
+temp_f = gets.chomp.to_i
+
+temp_c = temp_to_c(temp_f)
+
+puts "Your temperature is #{temp_c.to_string} degrees Celsius."
