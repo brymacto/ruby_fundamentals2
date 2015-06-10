5481e2a86d8cb0bcbccbb68de2b8fa730f5ffe27
Bryan <brymac@gmail.com>
2015-06-10 14:15:28 -0400

    Added rice, moved print into method, added number of items

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,16 @@
+def print_grocery_list(list)
+  list.each do |grocery|
+    puts "* #{grocery}"
+  end
+end
+
 grocery_list = ["coke", "chips", "pie", "candy"]
 
-grocery_list.each do |grocery|
+print_grocery_list(grocery_list)
 
-  puts "* #{grocery}"
+puts "Oops, forgot to add rice.  Adding it to the list now."
+grocery_list << "rice"
 
-end
+print_grocery_list(grocery_list)
+
+puts "You need to pick up #{grocery_list.length} items"
