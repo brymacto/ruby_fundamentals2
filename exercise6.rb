07be15f58b50ecca1d40874cf83293f2997abe69
Bryan <brymac@gmail.com>
2015-06-10 14:27:50 -0400

    Check for bananas, display second item in list

--- exercise6.rb
+++ exercise6.rb
@@ -6,11 +6,21 @@ end
 
 grocery_list = ["coke", "chips", "pie", "candy"]
 
+# Print
 print_grocery_list(grocery_list)
 
+# Add rice and print
 puts "Oops, forgot to add rice.  Adding it to the list now."
 grocery_list << "rice"
 
 print_grocery_list(grocery_list)
 
 puts "You need to pick up #{grocery_list.length} items"
+
+# Check for bananas
+
+puts (grocery_list.include?("bananas") ? "You don't need to pick up bananas today" : "You need to pick up bananas today")
+
+puts "The second item in the list is #{grocery_list[1]}"
+
+
