4930838bf6579489e7d4b8fe4f9a9d7efeaffb06
Bryan <brymac@gmail.com>
2015-06-10 15:18:47 -0400

    Increased all cohorts by 5%, deleted second cohort, prin total studentst

--- exercise7.rb
+++ exercise7.rb
@@ -4,6 +4,8 @@ def cohort_print(students)
   end
 end
 
+students_total = 0
+
 students = {
   cohort1: 34,
   cohort2: 42,
@@ -13,12 +15,25 @@ students = {
 cohort_print(students)
 
 puts "Adding cohort4"
-
 students[:cohort4] = 43
 
 puts "Keys are:"
-
 students.keys.each do |key|
   puts key
 end
 
+puts "Increasing all cohorts by 5%"
+students.each do |key, value|
+  students[key] = (value * 1.05).to_i
+end
+cohort_print(students)
+
+puts "Deleting the second cohort"
+students.delete(:cohort2)
+cohort_print(students)
+
+
+students.each_value do |value|
+  students_total += value
+end
+puts "Total students: #{students_total}"
