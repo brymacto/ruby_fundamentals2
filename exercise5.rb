5b6c19dfa90915d315d489be465e60dc2069adbf
Bryan <brymac@gmail.com>
2015-06-10 13:53:59 -0400

    Fixed float/integer issue line 2 (was returning zero for all inputted values)

--- exercise5.rb
+++ exercise5.rb
@@ -1,5 +1,5 @@
 def temp_to_c(temp_orig)
-  (temp_orig  - 32) * (5 / 9)
+  (temp_orig  - 32) * (5.0 / 9.0)
 end
 
 puts "Please enter a temperature in Farenheit"
@@ -7,4 +7,4 @@ temp_f = gets.chomp.to_i
 
 temp_c = temp_to_c(temp_f)
 
-puts "Your temperature is #{temp_c.to_s} degrees Celsius."
+puts "Your temperature is #{temp_c.to_i.to_s} degrees Celsius."
